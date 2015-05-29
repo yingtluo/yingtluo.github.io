e25fa33ffa667b777457c13810b3e9d987757a8d
Ying <ying.t.luo@gmail.com>
2015-05-28 21:20:02 -0700

    shipping version 1.0

--- js/script.js
+++ js/script.js
@@ -5,33 +5,37 @@ $(document).ready(function() {
 	var green = "#88ae60";
 	var pattern = "#fac564";
 
-	$(".reset").click(function() {
-		$("body").css({"background-image":"none", "background-color":"#fff"});
-	})
+	// $(".reset").click(function() {
+	// 	$("body").css({"background-image":"none", "background-color":"#fff"});
+	// })
 
 	$("#green").click(function() {
 		$("body").css({"background-image":"none", "background-color":green});
-		// changeColor(green);
+		$("#main-header").css("color", green);
+		$(".button").css("color", green);
+		$(".link").css("color", green);
+		// changeTextColor(green);
 	})
 
 	$("#pattern").click(function() {
 		$("body").css({"background-image":"url('img/food.png')", "background-color":pattern});
+		$("#main-header").css("color", pattern);
+		$(".button").css("color", pattern);
+		$(".link").css("color", pattern);
 		// changeTextColor(pattern);
 	})
 
 	$("#triangles").click(function() {
 		$("body").css({"background-image":"none", "background-color":"#fff"});
+		$("#main-header").css("color", "black");
+		$(".button").css("color", "black");
+		$(".link").css("color", "black");
 		// changeTextColor(triangles);
 	})
 
 	// function changeTextColor(color) {
 	// 	$("#main-header").css("color", color);
 	// 	$(".button").css("color", color);
+	//	$("a").css("color", color);
 	// })
-
-	// $('div#someDiv span').filter(function() {
-	//     var match = 'rgb(0, 0, 0)';
-	//     return ($(this).css('background-color') == match);
-	// }).css('background-color', currColor);
-
 });
